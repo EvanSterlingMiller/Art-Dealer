cca340bb0c806df08573fcd881cab83aca2d4289
Nestor <you@example.com>
2023-07-06 21:00:06 -0400

    more formal header and add css file for styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 const style = {
@@ -10,24 +10,32 @@ const style = {
 function Header({ currentPage, handlePageChange }) {
     return (
         <header>
-            <div>
- 
-             <Link to="/" style={style}>Artly</Link>   
-             <Link to="/Contact" style={style}> Contact Us</Link>
-
-             
-             <Link to="/Login" style={style}>Login</Link>
-
-             <Link to="/Cart" style={style}>Cart</Link>
-
+        
+            <div className='header_left'>
+        
+                <Link to="/Home">Artly</Link>
+                <Link to="/Contact" style={style}> Contact Us</Link>
+            
             </div>
+            
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500" rel="stylesheet"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500" rel="stylesheet"></link>
+            
+            <div className='header_right'>
+                
+                {/* profile pictute/avatar */}
+                
+                <Link to="/Login" style={style}>Login</Link>
+                <Link to="/Cart" style={style}>Cart</Link>
+            
+            </div>
+
+
         </header>
 
 
-    )
+)
 }
 
 export default Header; 
